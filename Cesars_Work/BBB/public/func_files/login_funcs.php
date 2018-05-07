4a863f4d11ed4c78f9f7920b75bcbbaffb8468c5
Cesar-Carrio <functioniskey@cesarjcarrillo.com>
2018-05-06 17:01:27 -0700

    modified login and signup function files to use prepared statements

--- Cesars_Work/BBB/public/func_files/login_funcs.php
+++ Cesars_Work/BBB/public/func_files/login_funcs.php
@@ -25,8 +25,8 @@ if (isset($_POST['submit'])){
 
     include '../../private/db_credentials.php';
 
-    $Username   = mysqli_real_escape_string($conn,$_POST['Username']);
-    $pwd        = mysqli_real_escape_string($conn, $_POST['pwd']);
+    $Username   = mysqli_real_escape_string($conn, htmlspecialchars($_POST['Username']));
+    $pwd        = mysqli_real_escape_string($conn, htmlspecialchars($_POST['pwd']));
 
     //error handlers
     //check if inputs are empty
@@ -34,29 +34,43 @@ if (isset($_POST['submit'])){
         header("Location: ../index.php?login=empty");
         exit();
     } else {
-        $sql = "select * from Accounts where Username='$Username'";
-        $result = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($result);
-        if ($resultCheck < 1){
-            header("Location: ../index.php?login=error");
-            exit();
-        } else {
-            if ($row = mysqli_fetch_assoc($result)){
-                //De-hashing
-                $hashedPwdCheck = password_verify($pwd, $row['PassHash']);
-                if ($hashedPwdCheck == false) {
-                    header("Location: ../index.php?login=error");
-                    exit();
-                } elseif($hashedPwdCheck == true) {
-                    //log in the user here
-                    $_SESSION['Username'] = $row['Username'];
-                    $_SESSION['first'] = $row['Name_First'];
-                    $_SESSION['last'] = $row['Name_Last'];
-                    $_SESSION['email'] = $row['Email'];
-                    $_SESSION['role'] = $row['Acc_role'];
-                    header("Location: ../Logged_In_Files/index.php?login=success");
-                    exit();
+        $sql = "select * from Accounts where Username=?";
 
+        //create a prepared statement
+        $stmt = mysqli_stmt_init($conn);
+        //prepare the prepared statement
+        if (!mysqli_stmt_prepare($stmt,$sql)){
+            echo "Sql statement failed!";
+        } else {
+            //bind params to the place holder
+            mysqli_stmt_bind_param($stmt, "s", $Username);
+            //Run params inside database
+            mysqli_stmt_execute($stmt);
+
+            //checks
+            $result = mysqli_stmt_get_result($stmt);
+            $resultCheck = mysqli_num_rows($result);
+            if ($resultCheck < 1){
+                header("Location: ../index.php?login=error");
+                exit();
+            } else {
+                if ($row = mysqli_fetch_assoc($result)){
+                    //De-hashing
+                    $hashedPwdCheck = password_verify($pwd, $row['PassHash']);
+                    if ($hashedPwdCheck == false) {
+                        header("Location: ../index.php?login=error");
+                        exit();
+                    } elseif($hashedPwdCheck == true) {
+                        //log in the user here
+                        $_SESSION['Username'] = $row['Username'];
+                        $_SESSION['first'] = $row['Name_First'];
+                        $_SESSION['last'] = $row['Name_Last'];
+                        $_SESSION['email'] = $row['Email'];
+                        $_SESSION['role'] = $row['Acc_role'];
+                        header("Location: ../Logged_In_Files/index.php?login=success");
+                        exit();
+
+                    }
                 }
             }
         }
--- Cesars_Work/BBB/public/func_files/signup_funcs.php
+++ Cesars_Work/BBB/public/func_files/signup_funcs.php
@@ -1,61 +1,84 @@
 <?php
 
-    if (isset($_POST['submit'])){
+if (isset($_POST['submit'])){
 
 
-        include_once '../../private/db_credentials.php';
+    include_once '../../private/db_credentials.php';
 
-        $Name_first     = mysqli_real_escape_string($conn, $_POST['Name_first']);
-        $Name_last      = mysqli_real_escape_string($conn, $_POST['Name_last']);
-        $Username       = mysqli_real_escape_string($conn, $_POST['Username']);
-        $Email          = mysqli_real_escape_string($conn, $_POST['Email']);
-        $pwd            = mysqli_real_escape_string($conn, $_POST['pwd']);
-        $Acc_role       = mysqli_real_escape_string($conn, 'observer');
-        //Any other account roles have to be added through either a user or manager
+    $Name_first     = mysqli_real_escape_string($conn, htmlspecialchars($_POST['Name_first']));
+    $Name_last      = mysqli_real_escape_string($conn, htmlspecialchars($_POST['Name_last']));
+    $Username       = mysqli_real_escape_string($conn, htmlspecialchars($_POST['Username']));
+    $Email          = mysqli_real_escape_string($conn, htmlspecialchars($_POST['Email']));
+    $pwd            = mysqli_real_escape_string($conn, htmlspecialchars($_POST['pwd']));
+    $Acc_role       = mysqli_real_escape_string($conn, 'observer');
+    //Any other account roles have to be added through either a user or manager
 
 
-        //error handlers
-        //check for empty fields
-        if (empty($Name_first) || empty($Name_last) || empty($Email) || empty($Username) || empty($pwd)){
-            header("Location: ../signup.php?signup=empty");
+    //error handlers
+    //check for empty fields
+    if (empty($Name_first) || empty($Name_last) || empty($Email) || empty($Username) || empty($pwd)){
+        header("Location: ../signup.php?signup=empty");
+        exit();
+    } else {
+        //check if input characters are valid
+        if (!preg_match("/^[a-zA-Z]*$/", $Name_first) || !preg_match("/^[a-zA-Z]*$/", $Name_last)){
+            header("Location: ../signup.php?signup=invalid");
             exit();
         } else {
-            //check if input characters are valid
-            if (!preg_match("/^[a-zA-Z]*$/", $Name_first) || !preg_match("/^[a-zA-Z]*$/", $Name_last)){
-                header("Location: ../signup.php?signup=invalid");
+            //check if email is valid
+            if (!filter_var($Email, FILTER_VALIDATE_EMAIL)){
+                header("Location: ../signup.php?signup=email");
                 exit();
             } else {
-                //check if email is valid
-                if (!filter_var($Email, FILTER_VALIDATE_EMAIL)){
-                    header("Location: ../signup.php?signup=email");
-                    exit();
+                //checking to see if username is taken
+                $sql = "SELECT * FROM Accounts WHERE Username = ?;";
+                //create a prepared statement
+                $stmt = mysqli_stmt_init($conn);
+                //prepare the prepared statement
+                if (!mysqli_stmt_prepare($stmt, $sql)){
+                    echo "Sql statement failed";
                 } else {
-                    //checking to see if username is taken
-                  $sql = "SELECT * FROM Accounts WHERE Username = '$Username';";
-                  $result = mysqli_query($conn, $sql);
-                  $resultCheck = mysqli_num_rows($result);
+                    //bind params to the place holder
+                    mysqli_stmt_bind_param($stmt, "s",$Username);
+                    //Run params inside database
+                    mysqli_stmt_execute($stmt);
 
-                  if ($resultCheck > 0){
-                      header("Location: ../signup.php?signup=usertaken");
-                      exit();
-                  } else {
-                      //hashing the password
-                      $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-                      //Inserting the user into the database
-                      //preventing sql injection using mysqli_real_escape_string() used above in code
-                      $sql = "insert into Accounts(Username, Acc_role, Email, PassHash, Name_First, Name_last) values ('$Username','$Acc_role','$Email','$hashedPwd','$Name_first','$Name_last');";
-                      mysqli_query($conn,$sql);
-                      header("Location: ../index.php?signup=success");
-                      exit();
+                    //checks
+                    $result = mysqli_stmt_get_result($stmt);
+                    $resultCheck = mysqli_num_rows($result);
 
-                  }
+                    if ($resultCheck > 0){
+                        header("Location: ../signup.php?signup=usertaken");
+                        exit();
+                    } else {
+                        //hashing the password
+                        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+                        //Inserting the user into the database
+                        //preventing sql injection using mysqli_real_escape_string() used above in code
+                        $sql = "insert into Accounts(Username, Acc_role, Email, PassHash, Name_First, Name_last) values (?,?,?,?,?,?);";
+
+                        $stmt = mysqli_stmt_init($conn);
+                        //prepare the prepared statement
+                        if (!mysqli_stmt_prepare($stmt,$sql)){
+                            echo "Sql statement failed";
+                        } else {
+                            //bind params to the place holders
+                            mysqli_stmt_bind_param($stmt,"ssssss", $Username,$Acc_role, $Email, $hashedPwd, $Name_first, $Name_last);
+                            //Run params inside database
+                            mysqli_stmt_execute($stmt);
+
+                            header("Location: ../index.php?signup=success");
+                            exit();
+                        }
+                    }
                 }
             }
         }
-
-    } else {
-        header("Location: ../signup.php");
-        exit();
     }
+
+} else {
+    header("Location: ../signup.php");
+    exit();
+}
 
 
